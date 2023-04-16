892292be44fcf33163f5e1cbecb059e5d8b0177b
dhnitesh <dhungananitesh18@gmail.com>
2023-04-17 00:00:07 +0545

    auth

--- jwt_project/urls.py
+++ jwt_project/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('profile/', include('profile.urls')),
+    path('user/', include('profile.urls')),
 
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
